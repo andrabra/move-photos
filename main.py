45decd53591cc9aed13e9ad38ac0f13e9d25d102
Andrew Abramov <andrabra@yandex.ru>
2023-10-25 22:51:13 +0500

    add readme.mdn

--- main.py
+++ main.py
@@ -1,43 +1,14 @@
 import shutil
 import os
 
-# Путь к исходной папке, где находятся файлы
-source_folder = 'D:\a'
+# Path to the source folder where the source_folder files
+source_folder = ''
 
-# Путь к папке, в которую нужно перенести файлы
-destination_folder = 'D:\b'
+# Path to the folder to move the files
+destination_folder = ''
 
-# Список файлов, которые нужно перенести
-files_to_move = ['IMG_5146.CR2',
-'IMG_5147.CR2',
-'IMG_5156.CR2',
-'IMG_5185.CR2',
-'IMG_5194.CR2',
-'IMG_5213.CR2',
-'IMG_5258.CR2',
-'IMG_5282.CR2',
-'IMG_5297.CR2',
-'IMG_5323.CR2',
-'IMG_5368.CR2',
-'IMG_5401.CR2',
-'IMG_5405.CR2',
-'IMG_5432.CR2',
-'IMG_5466.CR2',
-'IMG_5492.CR2',
-'IMG_5577.CR2',
-'IMG_5669.CR2',
-'IMG_5688.CR2',
-'IMG_5737.CR2',
-'IMG_5700.CR2',
-'IMG_5709.CR2',
-'IMG_5717.CR2',
-'IMG_5934.CR2',
-'IMG_5979.CR2',
-'IMG_6002.CR2',
-'IMG_6042.CR2',
-'IMG_6077.CR2',
-'IMG_6113.CR2',
-'IMG_6160.CR2']
+# List of files to transfer
+files_to_move = []
 
 for file_name in files_to_move:
     source_path = os.path.join(source_folder, file_name)
---
+++ readme.md
@@ -0,0 +1,17 @@
+# Move photos script
+
+## With this script, you can move files with specific names from the specified directory to another.
+
+It will be enough to change the path to the source and destination directories, as well as insert the necessary file names.
+
+## Example of code
+```
+# Path to the source folder where the source_folder files
+source_folder = ''
+
+# Path to the folder to move the files
+destination_folder = ''
+
+# List of files to transfer
+files_to_move = []
+```
